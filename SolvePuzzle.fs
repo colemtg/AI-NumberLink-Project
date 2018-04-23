a2d043b12e8b9f33bab0d2f5d35c13930efe0f85
colemtg <cole-pierce@uiowa.edu>
2018-04-23 09:04:16 -0500

    Probably will want several files

---
+++ SolvePuzzle.fs
@@ -0,0 +1,24 @@
+(*
+Project 1: Numberlink Puzzle
+Collin Kolars
+Cole Pierce
+Kenny Song
+
+
+TODO: Define the representation of a state of the puzzle
+- How to represent the variables: (current location, goal)?
+TODO: Read in a file or input of a puzzle and convert to an initial state
+TODO: Validate that an initial state is valid
+TODO: Validate that a solution state is a solution to an initial state
+TODO: Get next state function that gets all possible next states given a state
+TODO: Function that calculates the g(s) of a current state, or incorporate into the state
+TODO: Figure out what we want to use as our heuristic
+- Some combination of Manhattan distance and amount of next states a variables has?
+TODO: Implement greedy best first search
+TODO: Implement A* search
+TODO: Generate random puzzles
+TODO: Run tests on the random puzzles
+TODO: Implement improvements and run more tests
+
+
+*)
