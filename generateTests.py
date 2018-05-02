baf62441b9dd703036c0ba6d7a8acc8bcb5c92fc
Kenny  Song <kennysong@wireless-nat-inside.uiowa.edu>
2018-05-02 13:28:38 -0500

    clean up tests file

--- generateTests.py
+++ generateTests.py
@@ -17,43 +17,6 @@ def getRandomLetter(alist):
 def main():
 
 
-    # letterList = []
-    # alist = list("abcdefghijklmnopqrstuvwxyz")
-    # for letters in range(0, 7):
-    #     randomL = getRandomLetter(alist)
-    #     letterList.append(randomL)
-    #     alist.remove(randomL)
-    
-
-    # #letterListPair = copy.deepcopy(letterList)
-    # initBoard = []
-
-    # for t in range(0,8): #row
-    #     #indexOfLetter = randint(0,4)
-    #     #limit = False
-    #     for k in range(0,8): #column
-    #         #determine = len(initBoard) - 1
-    #         #if k == indexOfLetter:
-    #             #print letterList
-    #         if(len(letterList) > 0):
-    #             randomChoiceLL = random.choice(letterList)
-    #             #initBoard.count(randomChoiceLL)
-    #             print initBoard.count(randomChoiceLL)
-    #             if(initBoard.count(randomChoiceLL) < 2):
-    #                 initBoard.append(randomChoiceLL)
-    #                 #letterList.remove(randomChoiceLL)
-    #             else:
-    #                 while randomChoiceLL in letterList : letterList.remove(randomChoiceLL)
-    #             #elif(initBoard.count(randomChoiceLL) > 2):
-    #                 #initBoard.append(randomChoiceLL)
-                
-                    
-    #         else:
-    #             initBoard.append('0')
-       
-    # print(''.join(initBoard))
-    # masterList = []
-
    for m in range(8, 13): #8,13
         product = 5*m #5
         for g in range(0, product):
@@ -91,51 +54,6 @@ def main():
             print converted
             print ''
 
-
-    # for m in range(3, 5): #8,13
-    #     product = 2*m #5
-    #     for g in range(0, product):
-    #         n = randint(m-3,m)
-    #         letterList = []
-    #         for letters in range(0, n):
-    #             randomL = getRandomLetter()
-    #             if(randomL in letterList):
-    #                 letterList.append(randomL)
-    #             else:
-    #                 randomL2 = getRandomLetter()
-    #                 letterList.append(randomL2)
-
-    #         #letterListPair = copy.deepcopy(letterList)
-    #         initBoard = []
-
-    #         for t in range(0,m): #row
-    #             indexOfLetter = randint(0,m)
-    #             #limit = False
-    #             for k in range(0,m): #column
-    #                 #determine = len(initBoard) - 1
-    #                 if k == indexOfLetter:
-    #                     #print letterList
-    #                     if(len(letterList) > 0):
-    #                         randomChoiceLL = random.choice(letterList)
-    #                         #initBoard.count(randomChoiceLL)
-    #                         if(initBoard.count(randomChoiceLL) < 2):
-    #                             initBoard.append(randomChoiceLL)
-    #                             #letterList.remove(randomChoiceLL)
-                                
-
-    #                         elif(initBoard.count(randomChoiceLL) == 2):
-    #                             initBoard.append(randomChoiceLL)
-    #                             letterList.remove(randomChoiceLL)
-                                
-    #                     else:
-    #                         initBoard.append('0')
-    #                 else:
-    #                     initBoard.append('0')
-    #         converted = ''.join(initBoard)
-    #         sampleInput = (m, converted)
-    #         masterList.append(sampleInput)
-
-    # print masterList
 
 if __name__ == "__main__":
     main()
