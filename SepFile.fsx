de1fbc755567ab3c9eb5cc44bd8d9277905cc36a
Cole Pierce <31912242+colemtg@users.noreply.github.com>
2018-05-01 20:16:33 -0500

    See comments, should merge when all together

--- SepFile.fsx
+++ SepFile.fsx
@@ -1,34 +1,61 @@
 //Completed:
-// Take the input type of int string and convert to n by n char list
-// Take the input type of int string and convert to a map where the key
+// Take the input type of int * string and convert to n by n char list
+// Take the input type of int * string and convert to a map where the key
 //   is the char and the value is Type line where line has a char name,
 //   end position of the line, goal position of the line, and length of line 
 // not sure if we need length, but will make calculating cost of state easier I think
 // wasn't sure if should be initially 0 or 1 to begin with but shouldn't matter 
 //   as long as its consistant
+// Board validation
+// Checking if a state is a goal state
+// g(s) calculation for board
+// h(s) calculation for board
 
-type pos = int * int
+//TODO: integrate file reading and randomly generated boards
+//TODO: GetNextStates function for BoardState
+//TODO: do A* and greedy best
+
+type Pos = int * int
 
 //represents the different lines
-type line = {name: char; endPos: pos; goalPos: pos; length: int}
+type Line = {name: char; endPos: Pos; goalPos: Pos; length: int}
 with
   //updates the postion of the end of the line, adds one to length
-  member l.UpdateEndPos (p: pos) = {name = l.name; endPos = p; 
+  member l.UpdateEndPos (p: Pos) = {name = l.name; endPos = p; 
   goalPos = l.goalPos; length = l.length+1}
+
+  //manhatan distance of each line
+  member l.GetDistance = 
+    match (l.endPos, l.goalPos) with
+    (x1,y1),(x2,y2) -> abs (x1-x2) + abs (y1-y2)
+
+  member l.AtGoal = l.endPos = l.goalPos
 
 
 //update this with functions for getting next states, getting cost and heuristic
-type boardState = {size: int; lines: Map<char,line>; board: char list list}
+type BoardState = {size: int; lines: Map<char,Line>; board: char list list}
+with
 
+  //g(s) = sum of length of all lines in state s
+  member b.GetCost =
+    Map.fold (fun state _ value -> state + value.length) 0 b.lines
+
+  //h(s) = manhattan distance
+  member b.GetHeuristic = 
+    Map.fold (fun state _ (value:Line) -> state + value.GetDistance) 0 b.lines
+  
+  member b.AtGoal =
+    Map.fold(fun state _ (value:Line) -> state && value.AtGoal) true b.lines
+
+    
 
 //What the function that reads in puzzle from a file should return
-type fileInput = int * string
-
+type FileInput = int * string
 
 
 //takes in the file representation and converts to n by n char list
 //invalid check should be move elsewhere
-let rec convertToCharListList (f: fileInput) : char list list =
+let rec convertToCharListList (f: FileInput) : char list list =
   match f with
   (len, str) -> split len (Array.toList (str.ToCharArray())) 
 and split (n:int)(c: char list): char list list =
@@ -37,23 +64,23 @@ and split (n:int)(c: char list): char list list =
   | _ -> firstN n c :: split n (removeN n c)
 and firstN (n: int)(c: char list): char list =
   match c with
-  (x::xs) when n=1 -> [x]
+  (x::_) when n=1 -> [x]
   |(x::xs) -> x :: firstN (n-1) xs
   | _ -> []
 and removeN (n: int)(c: char list): char list =
   match c with
-  (x::xs) when n=1 -> xs
-  |(x::xs) -> removeN (n-1) xs
+  (_::xs) when n=1 -> xs
+  |(_::xs) -> removeN (n-1) xs
   | _ -> []
 
 
 //takes in the file input and outputs the map representation
 //should move the invalid check seperately
-let rec convertToMap (f: fileInput) : Map<char, line> =
+let rec convertToMap (f: FileInput) : Map<char, Line> =
   match f with
     (len, str)  -> addToMap len (Array.toList (str.ToCharArray()))
-and addToMap (n: int)(c: char list): Map<char, line> =
-  let mutable m = Map.empty<char,line>
+and addToMap (n: int)(c: char list): Map<char, Line> =
+  let mutable m = Map.empty<char,Line>
   for i = 0 to c.Length-1 do
     if c.[i] <> '0' && m.ContainsKey (c.[i]) then
      let tempLine = (Map.find c.[i] m)
@@ -66,7 +93,7 @@ and addToMap (n: int)(c: char list): Map<char, line> =
 //checks that a board is valid:
 // - sqrt string.size = length
 // - there are exactly 2 of each none '0' char
-let rec checkValidBoard(f: fileInput): bool =
+let rec checkValidBoard(f: FileInput): bool =
   match f with
   (_, str) -> checkValidInputSize f && checkTwoOfEach (List.sort (Array.toList (str.ToCharArray())))
 and checkTwoOfEach(c: char list) : bool = 
@@ -75,13 +102,13 @@ and checkTwoOfEach(c: char list) : bool =
   | (x :: y :: xs) when x ='0' -> checkTwoOfEach (y::xs)
   | ([_]) -> false
   | _ -> true
-and checkValidInputSize (f:fileInput): bool = 
+and checkValidInputSize (f:FileInput): bool = 
   match f with
   (len, str) when sqrt (double str.Length) = (double len) -> true
   | _ -> false 
   
 
-let constructInitialBoard (f:fileInput): boardState =
+let constructInitialBoard (f:FileInput): BoardState =
   match f with
   (len, _) when checkValidBoard f -> {size = len; lines = convertToMap f; board = convertToCharListList f}
   | _  -> failwith "invalidBoard"
@@ -89,3 +116,6 @@ let constructInitialBoard (f:fileInput): boardState =
 //test board
 let testInput = (4, "98980660zab0ab0z")
 let initialBoard = constructInitialBoard testInput
+initialBoard.GetCost
+initialBoard.GetHeuristic
+initialBoard.AtGoal
